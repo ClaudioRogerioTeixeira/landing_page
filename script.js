e4f58182c6471c33f7d087a1a9ff0510979d20a3
Teixeira <claudioteixeira@outlook.com>
2022-03-08 19:55:43 -0300

    Ajuste geral

--- script.js
+++ script.js
@@ -1,41 +1,97 @@
-let btnReserva = document.querySelector(".btn-reserva");
-let  inputNome  = document.querySelectorAll('#nome');
-console.log('inputNome: ', inputNome);
-console.log('inputNome: ', inputNome.values);
+const btnReserva = document.querySelector(".btn-reserva");
+const form = document.querySelector('form');
+const nome = document.querySelector('#nome');
+const tel = document.querySelector('#tel');
+const email = document.querySelector('#email');
+
+var smallNome  = document.querySelector('.smallNome');
+smallNome.style.visibility = "hidden";
+var smallTel  = document.querySelector('.smallTel');
+smallTel.style.visibility = "hidden";
+var smallEmail  = document.querySelector('.smallEmail');
+smallEmail.style.visibility = "hidden";
 
 function recaptchaCallback() {
-  alert('Acessou recaptcha...');
-  var btnReserva = document.querySelector('#btn-reserva');
-  btnReserva.removeAttribute('disabled');
-  btnReserva.style.cursor = 'pointer';
+  return new Promise(function() {
+      btnReserva.removeAttribute('disabled');
+      btnReserva.style.cursor = 'pointer';
+  }) 
 }
 
-btnReserva.addEventListener("click", function(event) { 
+btnReserva.addEventListener("click", function(event) {
+  'use strict';
   event.preventDefault();
+  const nomeValue = nome.value.trim();
+  const telValue = tel.value.trim();
+  const emailValue = email.value.trim();
 
-  let captcha = document.querySelector('.g-recaptcha');
-  console.log('captcha: ', captcha);
+  checkInputs();
 
-  Toastify({
-    text: "Enviado com Sucesso",
-    duration: 3000,
-    newWindow: true,
-    close: true,
-    gravity: "bottom", // `top` or `bottom`
-    position: "center", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "green",
-    }
-  }).showToast()
+  if (nomeValue != '' & telValue != '' & emailValue != '') {
+    Toastify({
+      text: "Enviado com Sucesso",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "bottom", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "green",
+      }
+    }).showToast()
 
-  // nome.value = '';
-  // tel.value = '';
-  // email.value = '';
+    // Testes limpar inputs
+    // nome.value = '';
+    // tel.value = '';
+    // email.value = '';
 
-  const inputs  = document.querySelectorAll('input');
-  for (let i = 0; i < inputs.length; i++) {
-    document.getElementById(inputs[i].name).value = '';
+    // const inputs  = document.querySelectorAll('input');
+    // for (let i = 0; i < inputs.length; i++) {
+    //   document.getElementById(inputs[i].name).value = '';
+    // }
+
+    setTimeout( () => {
+      form.submit();
+    }, 1000);
+    
   }
-  
 });
+
+function checkInputs() {
+  const nomeValue = nome.value.trim();
+  const telValue = tel.value.trim();
+  const emailValue = email.value.trim();
+  
+  if (nomeValue === '') {
+    errorValidation(nome, "Preencha o campo nome");
+  } else {
+    successValidation(nome);
+  }
+  if (telValue === '') {
+    errorValidation(tel, "Preencha o campo telefone");
+  } else {
+    successValidation(tel);
+  }
+  if (emailValue === '') {
+    errorValidation(email, "Preencha o campo email");
+  } else {
+    successValidation(email)
+  }
+}
+
+function errorValidation(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  small.style.visibility = "visible";
+  small.innerText = message;
+  formControl.className = 'form-control error';
+  return false;
+}
+
+function successValidation(input) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  small.style.visibility = "hidden";
+  formControl.className = 'form-control success';
+}
